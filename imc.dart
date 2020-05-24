d08bc397ee6e0563d391f6d2e2b2504dd673f707
Vanessa Silva Souto da Conceição <VenessaConceicao@PIMM-C02Z6A8YLVDC.local>
2020-05-23 23:24:56 -0300

    calculo do imc

--- imc.dart
+++ imc.dart
@@ -14,4 +14,19 @@ main(){
   // retornar imc
   print('O resultado do IMC é:');
   print(calc);
+
+  if(calc < 18.5){
+    print('Abaixo do peso');
+  } else if(calc > 18.5 && calc < 24.9){
+    print('Peso normal');
+  } else if(calc > 25 && calc < 29.9){
+    print('Sobrepeso');
+  } else if(calc > 30 && calc < 34.9){
+    print('Obessidade Grau 1');
+  } else if(calc > 35 && calc < 39.9){
+    print('Obessidade Grau 2');
+  } else {
+    print('Obessidade Grau 3');
+  }
+
 }
